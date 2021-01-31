e5a0856f66627330f2de1e0c15b35b3d38c8354c
Jackallster <77934946+Jackallster@users.noreply.github.com>
2021-01-31 21:23:47 +0300

    ClassMetot

---
+++ DortIslem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class DortIslem
+    {
+        public double islem(int secim,double s1,double s2)
+       {
+           double sonuc=0;
+           switch (secim)
+           {
+               case 0:
+                   Console.ReadKey();
+                   break;
+               case 1:
+                   sonuc = s1 + s2;
+                   break;
+
+               case 2:
+                   sonuc = s1 - s2;
+                   break;
+
+               case 3:
+                   sonuc = s1 * s2;
+                   break;
+
+               case 4:
+                   sonuc = s1 / s2;
+                   break;
+           }
+           return sonuc;
+           
+       }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s1, s2,i=0;
+            int secim;
+            do
+            {
+                Console.Write("bir sayı giriniz:");
+                s1 = double.Parse(Console.ReadLine());
+                Console.Write("bir sayı giriniz:");
+                s2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("------------------");
+                Console.WriteLine("1-toplama");
+                Console.WriteLine("2-çıkarma");
+                Console.WriteLine("3-çarpma");
+                Console.WriteLine("4-bölme");
+                Console.WriteLine("0-çıkış");
+                Console.Write("seçiminiz:");
+                secim = int.Parse(Console.ReadLine());
+                DortIslem dön = new DortIslem();
+                Console.WriteLine("\nsonucunuz=" + dön.islem(secim, s1, s2) + ("\n"));
+            } while (secim != 0);
+        }
+    }
+}
